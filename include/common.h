d3a4a67061a993cb09d243967b103efbbb374d22
akhilmysore <akhil@us129.sjc.aristanetworks.com>
2017-11-27 16:05:30 -0800

    Moving items from old repo

---
+++ include/common.h
@@ -0,0 +1,14 @@
+
+#ifndef HFILES_COMMON_H
+#define HFILES_COMMON_H
+
+typedef unsigned long unsint32_t;
+typedef unsigned short u_int16_t;
+typedef unsigned char u_int8_t;
+
+typedef void * voidp;
+
+#define trace printf
+
+#endif // HFILES_COMMON_H
+
